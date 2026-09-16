f42c82a9cc039bbdded4607740df1d2fb56afc84
jennetblom <jeboblom20@hotmail.com>
2026-09-16 14:02:51 +0200

    cleaned up code

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,7 +9,7 @@ function About() {
       <section className='aboutContainer'>
         <article className='aboutCard'>
           <img src={happy} id='profilePic' />
-          <p className='aboutCard-intro'>
+          <p className='boldtext'>
             Jag är en nyexaminerad mobilapplikationsutvecklare med erfarenhet av Android, iOS, React Native och webbutveckling i React.
             För mig är programmering ett spännande sätt att kombinera kreativitet och logik.
           </p>
@@ -17,7 +17,7 @@ function About() {
           <h2 className='aboutCardHeading'>💼 Erfarenhet och projekt</h2>
           <p className='aboutCardText'>
             Under utbildningen har jag arbetat praktiskt med flera projekt, både individuellt och i team.
-            I samarbeten trivs jag bra med att kommunicera, komma med idéer, lösa problem, stötta och bidra där det behövs för att projektet ska gå framåt och bli klart i tid. Jag trivs i arbetsmiljöer där det finns en öppen och positiv stämning, där man både kan skratta tillsammans och arbeta fokuserat mot gemensamma mål.
+            I samarbeten trivs jag bra med att komma med idéer, lösa problem, kommunicera, stötta och bidra där det behövs för att projektet ska gå framåt och bli klart i tid. Jag trivs i arbetsmiljöer där det finns en öppen och positiv stämning, där man både kan skratta tillsammans och arbeta fokuserat mot gemensamma mål.
           </p>
 
           <h2 className='aboutCardHeading'>⭐ Vad för jobb jag söker</h2>
@@ -27,7 +27,7 @@ function About() {
 
           <h2 className='aboutCardHeading'>🌿 Fritidsintressen</h2>
           <p className='aboutCardText'>
-            Utöver programmering tycker jag om att skapa musik, måla, läsa och röra på mig på olika sätt.
+            Utöver programmering tycker jag om att skapa musik, måla, spela och utforska skogen.
           </p>
           <Button label="Kontakta mig" to="/contact" />
         </article>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,22 @@
 import profilbild from '../../assets/profilbild.jpg';
 import './Home.css'
-import rectangle from '../../assets/Rectangle.png';
 import workplace from '../../assets/workplace.png';
-import clickSound from "../../assets/clicksound.wav";
-import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button.jsx';
 function Startpage() {
 
-    const navigate = useNavigate();
 
-/*     const handleClick = () => {
-        const audio = new Audio(clickSound);
-        audio.volume = 0.2;
-        audio.play();
-        navigate('/projects');
-    }; */
     return (
 
         <main className='startpage'>
             <section className='card'>
 
                 <header className='nameInfo'>
-                    <p id='intro'>
+                    <p className="cardSubtitle">
                         Hej, mitt namn är
                     </p>
                     <h1 id='name'>Jennet Blom</h1>
                 </header>
-                <p id='description'>
+                <p>
                     Jag är en nyexaminerad apputvecklare som gillar att vara kreativ och påhittig.
                     <br />
                     Har erfarenhet av både mobila plattformar och crossplattformutveckling.
